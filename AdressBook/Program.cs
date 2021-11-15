a99cc3a096e3b3d5b5d36587c00da0c768fdf301
“Arunesh <as8175802986@gmail.com>
2021-11-15 23:54:30 +0530

    [Arunesh] Added: for Adressbook

---
+++ AdressBook/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Address Book Program");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact details in specified format");
+                        Contacts contact = new Contacts()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
